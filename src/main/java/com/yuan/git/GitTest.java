34514927ce4bb95eb979a90fff9fb59e2d507aa2
yuanaux <106010797+yuanaux@users.noreply.github.com>
2022-05-22 20:03:51 +0800

    pull commit

--- src/main/java/com/yuan/git/GitTest.java
+++ src/main/java/com/yuan/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git！ main");
         System.out.println("hello git！ hot-fix");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
